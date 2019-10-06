0bd43d1af56fa3d8184553a9ec39a1f313a5b2a1
Charles Willian <wcharlesw15@gmail.com>
2019-10-06 10:58:27 -0300

    Solution added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ValidationComponent from './ValidationComponent/ValidationComponent';
-import CharComponent from './CharComponent/CharComponent';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
-    textLength : 0,
-    text: []
+    userInput: ''
   }
 
-  showLengthHandler = event => this.setState({textLength: event.target.value.length});
-  
-  textHandler = event => this.setState({text: event.target.value.split('')});
-
-  deleteCharHandler = charIndex =>{
-    let textArr = [...this.state.text];
-
-    textArr.splice(charIndex,1);
-    this.setState({text: textArr});
+  inputChangedHandler = ( event ) => {
+    this.setState( { userInput: event.target.value } );
   }
 
-  render() {
-    
-    let chars = (
-      <div>
-        {this.state.text.map((char, index) => {
-          return(
-            <CharComponent 
-              key = {index}
-              char={char}
-              delete={() => this.deleteCharHandler(index)}
-            />
-          )
-         })
-        }
-      </div>
-    )
-    
+  deleteCharHandler = ( index ) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+
+  render () {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char 
+        character={ch} 
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;
+    });
+
     return (
       <div className="App">
-        <input type="text" 
-        onChange={(event)=>{this.showLengthHandler(event); this.textHandler(event)}}/>
-        <p>{this.state.textLength}</p>
-        <ValidationComponent
-          textLength = {this.state.textLength}
-        />
-        {chars}
+        <ol>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+        </ol>
+        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+        <hr />
+        <input
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
